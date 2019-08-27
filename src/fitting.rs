3d2208b8127c08b6e39e63303e2874945cf30f57
jatentaki <michal.tyszkiewicz@gmail.com>
2019-08-27 13:32:46 +0200

    implement build_m and build_embedding

--- src/fitting.rs
+++ src/fitting.rs
@@ -1,6 +1,6 @@
 use crate::{CubicBez, Point, Line, ParamCurveFit};
 
-use nalgebra::{U1, U2, U4, Dynamic, Matrix, VecStorage, Matrix4, linalg::LU};
+use nalgebra::{U1, U2, U4, U8, Dynamic, Matrix, VecStorage, Matrix4, linalg::LU};
 use lazy_static::lazy_static;
 
 type VMatrix<Col, Row> = Matrix<f64, Col, Row, VecStorage<f64, Col, Row>>;
@@ -100,5 +100,218 @@ impl ParamCurveFit for CubicBez {
         //dbg!(&u, &s, &v_t);
 
         initial_guess_inner(points)
+    }
+}
+
+/// Represents the three possible types of constraints imposed on control points
+/// when fitting curves
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum Constraint {
+    /// The control point is unconstrained
+    Free,
+    /// The control point is fixed to be exactly equal to `Point`
+    Fixed(Point),
+    /// The control point is constrained to lie on the line
+    /// given by `a` * x + `b` * y + `c` = 0
+    Line(f64, f64, f64),
+}
+
+impl Constraint {
+    fn is_fixed(&self) -> bool {
+        if let Constraint::Fixed(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn initial_guess(points: &[Point]) -> CubicBez {
+    let p0 = points.first().expect("failed to fetch the first point");
+    let pn = points.last().expect("failed to fetch the last point");
+
+    Line::new(*p0, *pn).into()
+}
+
+type DMatrix = VMatrix<Dynamic, Dynamic>;
+
+lazy_static! {
+    static ref M_8: DMatrix = {
+        // This matrix is actually block-diagonal and could be simplified as
+        //
+        // M_8 = [[M, 0],
+        //        [0, M]]
+        //
+        // where M is the matrix defined in
+        // https://pomax.github.io/bezierinfo/#curvefitting
+        DMatrix::from_row_slice(8, 8, &[
+             1.,  0.,  0.,  0.,    0.,  0.,  0.,  0.,
+            -3.,  3.,  0.,  0.,    0.,  0.,  0.,  0.,
+             3., -6.,  3.,  0.,    0.,  0.,  0.,  0.,
+            -1.,  3., -3.,  1.,    0.,  0.,  0.,  0.,
+
+             0.,  0.,  0.,  0.,    1.,  0.,  0.,  0.,
+             0.,  0.,  0.,  0.,   -3.,  3.,  0.,  0.,
+             0.,  0.,  0.,  0.,    3., -6.,  3.,  0.,
+             0.,  0.,  0.,  0.,   -1.,  3., -3.,  1.,
+        ])
+    };
+}
+
+fn build_m(constraints: &[Constraint; 4], fixed: bool) -> DMatrix {
+    let mut columns = Vec::<usize>::with_capacity(8);
+
+    // pick X coordinate columns (0..4)
+    for (i, constr) in constraints.iter().enumerate() {
+        if fixed == constr.is_fixed() {
+            columns.push(i);
+        }
+    }
+
+    // pick Y coordinate columns (4..8)
+    for (i, constr) in constraints.iter().enumerate() {
+        if fixed == constr.is_fixed() {
+            columns.push(i + 4);
+        }
+    }
+    
+    M_8.select_columns(columns.iter())
+}
+
+fn build_embedding(constraints: &[Constraint; 4]) -> DMatrix {
+    use Constraint::*;
+
+    let mut n_dof = 0;
+    let mut n_fit = 0;
+    for constr in constraints {
+        match constr {
+            Fixed(_)   => continue,
+            Line(_, _, _) => {
+                n_dof += 1;
+                n_fit += 2;
+            },
+            Free       => {
+                n_dof += 2;
+                n_fit += 2;
+            },
+        };
+    };
+
+    let mut embedding = DMatrix::zeros(n_fit, n_dof);
+
+    let mut row = 0;
+    let mut col = 0;
+    for constr in constraints {
+        match constr {
+            Fixed(_)   => continue,
+            Line(x, y, _a) => {
+                *embedding.index_mut((row, col)) = *x;
+                *embedding.index_mut((row + 1, col)) = *y;
+                row += 2;
+                col += 1;
+            },
+            Free       => {
+                *embedding.index_mut((row, col)) = 1.;
+                *embedding.index_mut((row + 1, col + 1)) = 1.;
+                row += 2;
+                col += 2;
+            },
+        };
+    };
+
+    embedding
+}
+
+//fn build_p_offset(constraints: &[Constraint; 4]) -> DMatrix {
+//    
+//}
+
+fn fit_with_t(points: &[Point], ts: &[f64], constraints: [Constraint; 4]) -> CubicBez {
+    
+
+
+    unimplemented!();
+}
+
+mod test {
+    use crate::fitting::{
+        M_8, Constraint, DMatrix, build_m, build_embedding
+    };
+    use Constraint::*;
+
+    #[test]
+    fn test_build_m_free() {
+        let constraints = [
+            Fixed((0., 0.).into()),
+            Line(1., 0., 2.),
+            Free,
+            Fixed((0., 1.).into()),
+        ];
+
+        let m_free = build_m(&constraints, false);
+
+        let m_free_expected = DMatrix::from_row_slice(8, 4, &[
+             0.,  0.,     0.,  0.,
+             3.,  0.,     0.,  0.,
+            -6.,  3.,     0.,  0.,
+             3., -3.,     0.,  0.,
+
+             0.,  0.,     0.,  0.,
+             0.,  0.,     3.,  0.,
+             0.,  0.,    -6.,  3.,
+             0.,  0.,     3., -3.,
+        ]);
+
+        assert_eq!(m_free, m_free_expected);
+    }
+
+    #[test]
+    fn test_build_m_fixed() {
+        let constraints = [
+            Fixed((0., 0.).into()),
+            Line(1., 0., 2.),
+            Free,
+            Fixed((0., 1.).into()),
+        ];
+
+        let m_fixed = build_m(&constraints, true);
+
+        let m_fixed_expected = DMatrix::from_row_slice(8, 4, &[
+             1.,  0.,    0.,  0.,
+            -3.,  0.,    0.,  0.,
+             3.,  0.,    0.,  0.,
+            -1.,  1.,    0.,  0.,
+
+             0.,  0.,    1.,  0.,
+             0.,  0.,   -3.,  0.,
+             0.,  0.,    3.,  0.,
+             0.,  0.,   -1.,  1.,
+        ]);
+
+        assert_eq!(m_fixed, m_fixed_expected);
+    }
+
+    #[test]
+    fn test_build_embedding() {
+        let constraints = [
+            Fixed((0., 0.).into()),
+            Line(1., 0., 2.),
+            Free,
+            Fixed((0., 1.).into()),
+        ];
+
+        let embedding = build_embedding(&constraints);
+
+        let embedding_expected = DMatrix::from_row_slice(4, 3, &[
+            1., 0., 0.,
+            0., 0., 0.,
+            0., 1., 0.,
+            0., 0., 1.,
+        ]);
+
+        println!("{}", &embedding);
+        println!("{}", &embedding_expected);
+
+        assert_eq!(embedding, embedding_expected);
     }
 }
